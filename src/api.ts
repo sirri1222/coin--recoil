a877466b6959d00f4e5f79d3eaa82a2b3f67217e
sirri1222 <hyeonji159753@gmail.com>
2023-04-10 11:03:12 +0900

    차트 진행중

---
+++ src/api.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const BASE_URL = "https://api.coinpaprika.com/v1";
+// const BASE_URL = "temp";
+export const fetchCoins = async () => {
+  return await axios
+    .get(`${BASE_URL}/coins`)
+    .then((res) => res.data.slice(0, 100));
+};
+
+export const fetchCoinInfo = async (coinId: string) => {
+  return await axios //
+    .get(`${BASE_URL}/coins/${coinId}`) //
+    .then((res) => res.data);
+};
+
+export const fetchCoinTickers = async (coinId: string) => {
+  return await axios
+    .get(`${BASE_URL}/tickers/${coinId}`)
+    .then((res) => res.data);
+};
+
+export const fetchCoinHistory = async (coinId: string) => {
+  return await axios
+    .get(`https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`)
+    .then((res) => res.data);
+};
