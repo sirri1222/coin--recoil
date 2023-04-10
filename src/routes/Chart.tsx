ffca7cc65901d8c63363321726ddf391e908371a
sirri1222 <hyeonji159753@gmail.com>
2023-04-10 11:01:05 +0900

    리엑트 쿼리, 리코일, 차트 라이브러리 설치

---
+++ src/routes/Chart.tsx
@@ -0,0 +1,86 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { useOutletContext } from "react-router-dom";
+import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "./atoms";
+
+interface IHistorical {
+  close: string;
+  high: string;
+  low: string;
+  market_cap: number;
+  open: string;
+  time_close: number;
+  time_open: number;
+  volume: string;
+}
+interface ChartProps {
+  coinId: string;
+}
+export default function Chart() {
+  const { coinId } = useOutletContext<ChartProps>();
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
+    fetchCoinHistory(coinId)
+  );
+
+  // console.log(data?.map((price) => price.close));
+
+  const isDark = useRecoilValue(isDarkAtom);
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price : any) => parseFloat(price.close)) ?? [],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: isDark ? "dark" : "light",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              background: "transparent",
+              toolbar: {
+                show: false,
+              },
+            },
+            grid: { show: false }, //선
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              show: false,
+            },
+            xaxis: {
+              axisBorder: { show: false },
+              axisTicks: { show: false },
+              labels: { show: false },
+              type: "datetime",
+              categories: data?.map((price : any) =>
+                new Date(price.time_close * 1000).toISOString()
+              ),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9  "],
+            tooltip: {
+              y: { formatter: (value) => `$ ${value.toFixed(2)}` },
+            },
+          }}
+        />
+      )}
+    </div>
+  );
+}
